859f627dc40b95695f8a3a2d9530cf3d01fef311
cyklon83 <mmarczewski83@wp.pl>
2016-11-21 10:17:45 +0100

    commit from command line on master

--- com/xyz/util/DateUtil.java
+++ com/xyz/util/DateUtil.java
@@ -6,7 +6,7 @@ import org.apache.commons.lang.time.DateFormatUtils;
 public class DateUtil {
 
         public static String getToday() {
-		//marczewski
+		//marczewski from master
 
                 String today = DateFormatUtils.format(new Date(), "dd-MMM-yyyy");
             //marczewski 2
